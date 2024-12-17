3838058d053632efb2a35b99794cb4443b6152c0
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-12-17 23:13:43 +0700

    HomePage

--- client/src/assets/Pages/HomePage.jsx
+++ client/src/assets/Pages/HomePage.jsx
@@ -8,24 +8,24 @@ function HomePage() {
 
     const fetchPokemon = async () => {
         try {
-            const { data } = await PokeAPI.get('/pokemon');
+            const { data } = await PokeAPI.get('/pokemon')
             const pokemonsPromises = data.results.map(async (pokemon) => {
-                const pokemonId = pokemon.url.split('/')[6];
-                return fetchPokemon2(pokemonId);
-            });
-            const pokemonsResponses = await Promise.all(pokemonsPromises);
-            const pokemonsData = pokemonsResponses.map((response) => response.data);
-            setPokemons(pokemonsData);
+                const pokemonId = pokemon.url.split('/')[6]
+                return fetchPokemon2(pokemonId)
+            })
+            const pokemonsResponses = await Promise.all(pokemonsPromises)
+            const pokemonsData = pokemonsResponses.map((response) => response.data)
+            setPokemons(pokemonsData)
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
     }
 
     const fetchPokemon2 = async (pokemonId) => {
         try {
-            return await PokeAPI.get(`/pokemon/${pokemonId}`);
+            return await PokeAPI.get(`/pokemon/${pokemonId}`)
         } catch (err) {
-            console.log(err);
+            console.log(err)
         }
     }
     useEffect(() => {
@@ -36,7 +36,7 @@ function HomePage() {
             <div>HomePage</div>
             <div>Search Form</div>
             <div>Filter</div>
-            <CardsPokemons pokemons={pokemons} />
+            <CardsPokemons pokemons={pokemons}/>
             <div>Footer</div>
         </div>
 
