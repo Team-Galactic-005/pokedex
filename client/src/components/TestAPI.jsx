c0f29d1de483f0bf869ad587267372f8c8094fb7
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-12-17 11:31:04 +0700

    repair fetch data pokemons

--- client/src/components/TestAPI.jsx
+++ client/src/components/TestAPI.jsx
@@ -3,44 +3,57 @@ import PokeAPI from '../library/axios'
 
 function TestAPI() {
     const [pokemons, setPokemons] = useState([])
+    console.log(pokemons)
 
     const fetchPokemon = async () => {
         try {
-            const { data } = await PokeAPI.get('/pokemon')
-            data.results?.forEach((pokemon) => {
-                const pokemonId = pokemon.url.split('/')[6]
-                fetchPokemon2(pokemonId)
-            })
+            const { data } = await PokeAPI.get('/pokemon');
+            const pokemonsPromises = data.results.map(async (pokemon) => {
+                const pokemonId = pokemon.url.split('/')[6];
+                return fetchPokemon2(pokemonId);
+            });
+            const pokemonsResponses = await Promise.all(pokemonsPromises);
+            const pokemonsData = pokemonsResponses.map((response) => response.data);
+            setPokemons(pokemonsData);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
 
     const fetchPokemon2 = async (pokemonId) => {
         try {
-            const { data } = await PokeAPI.get(`/pokemon/${pokemonId}`)
-            setPokemons([...pokemons, data])
+            return await PokeAPI.get(`/pokemon/${pokemonId}`);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     }
     useEffect(() => {
         fetchPokemon()
     }, [])
     return (
-        <div>
-            {pokemons?.map((pokemon) => (
-                <div key={pokemon.id}>
-                    <span>{pokemon.url}</span>
-                    <h1>{pokemon.name}</h1>
-                    <img src='' alt=''/>
-                </div> 
-            ))}
-            <div>
-                <span></span>
-                <h1></h1>
-                <img src='' alt=''/>
-            </div> 
+        <div className='container mx-auto'>
+            <div className='grid grid-cols-3 text-center'>
+                {pokemons?.map((pokemon) => (
+                    <div key={pokemon.id} className='border'>
+                        <div className=''>
+                            <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                        </div>
+                        <div>
+                            <span>{pokemon.id}</span>
+                            <h1>{pokemon.name}</h1>
+                            <div className='flex justify-around'>
+                                {pokemon.types?.map((type) => (
+                                    <div key={type.type.name}>
+                                        <h1>{type.type.name}</h1>
+                                    </div>
+                                ))}
+                            </div>
+
+                        </div>
+                    </div>
+                ))}
+            </div>
+
         </div>
     )
 }
