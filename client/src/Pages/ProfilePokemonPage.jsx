719d9cfd3f443ff2bc3b1989eb90f142a9348bd6
dindanrml <dindanrml2444@gmail.com>
2024-12-20 14:09:47 +0700

    fixing margin height weight section

--- client/src/Pages/ProfilePokemonPage.jsx
+++ client/src/Pages/ProfilePokemonPage.jsx
@@ -1,187 +1,228 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
-import PokeAPI from '../library/axios'
-import bg from '../assets/main_bg_v15_1.jpg'
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import PokeAPI from "../library/axios";
+import bg from "../assets/main_bg_v15_1.jpg";
 
 const colorButton = {
-    normal: 'bg-[#A0A0A0] border-[#DCDCDC]',
-    fighting: 'bg-[#C85500] border-[#DC6900]',
-    flying: 'bg-[#79BCD7] border-[#78DCFF]',
-    poison: 'bg-[#BE78BE] border-[#D28CD2]',
-    ground: 'bg-[#CCA142] border-[#F9C75A]',
-    rock: 'bg-[#A07850] border-[#B48C64]',
-    bug: 'bg-[#32B432] border-[#46C846]',
-    ghost: 'bg-[#8C78F0] border-[#A08CFF]',
-    steel: 'bg-[#96B4DC] border-[#AAC8F0]',
-    fire: 'bg-[#FF3700] border-[#FF6900]',
-    water: 'bg-[#0094E5] border-[#14B9FF]',
-    grass: 'bg-[#92BF19] border-[#B4F000]',
-    electric: 'bg-[#E4B700] border-[#FFE100]',
-    psychic: 'bg-[#DC78C8] border-[#EF8BDB]',
-    ice: 'bg-[#00B7EE] border-[#14F5FF]',
-    dragon: 'bg-[#3C64C8] border-[#5078DC]',
-    dark: 'bg-[#606060] border-[#787878]',
-    fairy: 'bg-[#FF7EB8] border-[#FDAEDA]',
-    stellar: 'bg-[#A0A0A0]',
-    shadow: 'bg-[#A0A0A0]',
-    unknown: 'bg-[#A0A0A0]'
-}
+  normal: "bg-[#A0A0A0] border-[#DCDCDC]",
+  fighting: "bg-[#C85500] border-[#DC6900]",
+  flying: "bg-[#79BCD7] border-[#78DCFF]",
+  poison: "bg-[#BE78BE] border-[#D28CD2]",
+  ground: "bg-[#CCA142] border-[#F9C75A]",
+  rock: "bg-[#A07850] border-[#B48C64]",
+  bug: "bg-[#32B432] border-[#46C846]",
+  ghost: "bg-[#8C78F0] border-[#A08CFF]",
+  steel: "bg-[#96B4DC] border-[#AAC8F0]",
+  fire: "bg-[#FF3700] border-[#FF6900]",
+  water: "bg-[#0094E5] border-[#14B9FF]",
+  grass: "bg-[#92BF19] border-[#B4F000]",
+  electric: "bg-[#E4B700] border-[#FFE100]",
+  psychic: "bg-[#DC78C8] border-[#EF8BDB]",
+  ice: "bg-[#00B7EE] border-[#14F5FF]",
+  dragon: "bg-[#3C64C8] border-[#5078DC]",
+  dark: "bg-[#606060] border-[#787878]",
+  fairy: "bg-[#FF7EB8] border-[#FDAEDA]",
+  stellar: "bg-[#A0A0A0]",
+  shadow: "bg-[#A0A0A0]",
+  unknown: "bg-[#A0A0A0]",
+};
 
 function ProfilePokemonPage() {
-    const params = useParams()
-    const pokemonId = params.idPokemon
+  const params = useParams();
+  const pokemonId = params.idPokemon;
 
-    const [isLoading, setIsLoading] = useState(false)
-    const [error, setError] = useState(null)
-    const [profilePokemon, setProfilePokemon] = useState([])
-    console.log(profilePokemon)
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [profilePokemon, setProfilePokemon] = useState([]);
+  console.log(profilePokemon);
 
-    const setButtonColor = (type) => {
-        return colorButton[type]
+  const setButtonColor = (type) => {
+    return colorButton[type];
+  };
+
+  const fetchProfilePokemon = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const { data } = await PokeAPI.get(`/pokemon/${pokemonId}`);
+      setProfilePokemon(data);
+    } catch (error) {
+      console.error(err);
+      setError(err);
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    const fetchProfilePokemon = async () => {
-        setIsLoading(true)
-        setError(null)
-        try {
-            const { data } = await PokeAPI.get(`/pokemon/${pokemonId}`)
-            setProfilePokemon(data)
-        } catch (error) {
-            console.error(err)
-            setError(err)
-        } finally {
-            setIsLoading(false)
-        }
-    }
+  useEffect(() => {
+    fetchProfilePokemon();
+  }, []);
 
-    useEffect(() => {
-        fetchProfilePokemon()
-    }, [])
+  const formattedId = profilePokemon.id
+    ? String(profilePokemon.id).padStart(4, "0")
+    : "";
+  const formattedName = profilePokemon.name
+    ? String(profilePokemon.name).charAt(0).toUpperCase() +
+      profilePokemon.name.slice(1).toLowerCase()
+    : "";
+  const formattedHeight = profilePokemon.height
+    ? (profilePokemon.height / 10).toFixed(1)
+    : "0.0";
+  const formattedWeight = profilePokemon.weight
+    ? (profilePokemon.weight / 10).toFixed(1)
+    : "0.0";
 
-    const formattedId = profilePokemon.id ? String(profilePokemon.id).padStart(4, '0') : ''
-    const formattedName = profilePokemon.name ? String(profilePokemon.name).charAt(0).toUpperCase() + profilePokemon.name.slice(1).toLowerCase() : ''
-    const formattedHeight = profilePokemon.height ? (profilePokemon.height / 10).toFixed(1) : '0.0'
-    const formattedWeight = profilePokemon.weight ? (profilePokemon.weight / 10).toFixed(1) : '0.0'
-
-    return (
-        <>
-            <div className='container mx-auto text-white my-5 grid gap-y-32'>
-                <div>
-                    <div className='flex justify-center relative'>
-                        <img className='absolute -z-20' src={bg} alt='' />
-                    </div>
-                    <div className='flex justify-center mb-5 mt-11'>
-                        <Link to='/'><h1 className='text-3xl text-black'>Pokédex</h1></Link>
-                    </div>
-                    <div className='flex justify-between mt-14'>
-                        <div>
-
-                        </div>
-                        <div className='text-center'>
-                            <h1 className='text-4xl text-[#B3EAFE]'>{formattedId}</h1>
-                            <h1 className='text-5xl text-[#FFFFFF]'>{formattedName}</h1>
-                        </div>
-                        <div>
-
-                        </div>
-                    </div>
-                    <div className='grid grid-cols-3 mt-14'>
-                        <div className='grid justify-center'>
-                            <div className='text-center'>
-                                <h1 className='text-3xl mb-5'>Type</h1>
-                                {profilePokemon.types?.map((type) => {
-                                    const formattedType = type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1).toLowerCase()
-                                    return (
-                                        <p className={`text-lg border py-1 px-40 mb-5 rounded-full ${setButtonColor(type.type.name)}`} key={type.slot}>{formattedType}</p>
-                                    )
-                                })}
-                                {profilePokemon.types?.length === 1 && (
-                                    <div className="py-[19px] px-40 mb-5">
-                                    </div>
-                                )}
-                            </div>
-                            <div className='text-center'>
-                                <h1 className='text-3xl mb-5'>Weakness</h1>
-                                <div className='grid grid-rows-2 text-center gap-y-5'>
-                                    <div className='grid grid-cols-2 gap-x-3'>
-                                        <p className='border rounded-full py-1'>Fire</p>
-                                        <p className='border rounded-full py-1'>Ice</p>
-                                    </div>
-                                    <div className='grid grid-cols-2 gap-x-3'>
-                                        <p className='border rounded-full py-1'>Flying</p>
-                                        <p className='border rounded-full py-1'>Psychic</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='relative flex justify-center items-center top-16 -z-10'>
-                            <img className='absolute animate-spin-custom' src='https://id.portal-pokemon.com/play/resources/pokedex/img/pokemon_bg.png' alt='' />
-                            <img className='absolute w-[350px]' src='https://id.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png' alt='' />
-                            <img className='absolute w-[50%]' src={profilePokemon.sprites?.other['official-artwork']?.front_default} alt={profilePokemon.name} />
-                        </div>
-                        <div className='grid text-xl px-10 mt-20 me-20'>
-                            <div className='grid grid-cols-2'>
-                                <div>
-                                    <p className='text-[#B0E5F9]'>Height</p>
-                                    <p>{formattedHeight} m</p>
-                                </div>
-                                <div>
-                                    <p className='text-[#B0E5F9]'>Base Experience</p>
-                                    <p>{profilePokemon.base_experience}</p>
-                                </div>
-                            </div>
-                            <div className='grid grid-cols-2'>
-                                <div>
-                                    <p className='text-[#B0E5F9]'>Weight</p>
-                                    <p>{formattedWeight} kg</p>
-                                </div>
-                                <div>
-                                <p className='text-[#B0E5F9]'>Abilities</p>
-                                {profilePokemon.abilities?.map((ability) => {
-                                    const formattedAbility = ability.ability.name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
-                                    return (
-                                        <p key={ability.ability.name}>{formattedAbility}</p>
-                                    )
-                                })}
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-                </div>
-                <div className='grid grid-cols-3'>
-                    <div className='ps-20'>
-                        <h1 className='text-3xl text-[#B0E5F9]'>Version</h1>
-                        <p className='text-xl mt-5'>Mandi cahaya matahari membuat Ivysaur makin kuat dan menumbuhkan kuncup di punggungnya.</p>
-                    </div>
-                    <div>
-
-                    </div>
-                    <div>
-                        <h1 className='text-3xl text-[#B0E5F9] mt-[2px]'>Statistics</h1>
-                        <div className='mt-5'>
-                            {profilePokemon.stats?.map((stat) => {
-                                const formattedStats = stat.stat.name.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ')
-                                return (
-                                    <div className='flex gap-3 text-xl' key={stat.stat.name}>
-                                        <h1>{formattedStats}</h1>
-                                        <p>{stat.base_stat}</p>
-                                    </div>
-                                )
-                            })}
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-center'>
-                    <div className='text-center border border-[#466E9B] rounded-full w-[50%] text-lg text-[#b3eafe] hover:bg-[#B4EBFF] hover:text-black hover:border-transparent'>
-                        <Link to='/'><button className='w-full p-2'>Back to Home Page</button></Link>
-                    </div>
-                </div>
-                
+  return (
+    <>
+      <div className="container mx-auto text-white my-5 grid gap-y-32">
+        <div>
+          <div className="flex justify-center relative">
+            <img className="absolute -z-20" src={bg} alt="" />
+          </div>
+          <div className="flex justify-center mb-5 mt-11">
+            <Link to="/">
+              <h1 className="text-3xl text-black">Pokédex</h1>
+            </Link>
+          </div>
+          <div className="flex justify-between mt-14">
+            <div></div>
+            <div className="text-center">
+              <h1 className="text-4xl text-[#B3EAFE]">{formattedId}</h1>
+              <h1 className="text-5xl text-[#FFFFFF]">{formattedName}</h1>
             </div>
-        </>
-
-    )
+            <div></div>
+          </div>
+          <div className="grid grid-cols-3 mt-14">
+            <div className="grid justify-center">
+              <div className="text-center">
+                <h1 className="text-3xl mb-5">Type</h1>
+                {profilePokemon.types?.map((type) => {
+                  const formattedType =
+                    type.type.name.charAt(0).toUpperCase() +
+                    type.type.name.slice(1).toLowerCase();
+                  return (
+                    <p
+                      className={`text-lg border py-1 px-40 mb-5 rounded-full ${setButtonColor(
+                        type.type.name
+                      )}`}
+                      key={type.slot}
+                    >
+                      {formattedType}
+                    </p>
+                  );
+                })}
+                {profilePokemon.types?.length === 1 && (
+                  <div className="py-[19px] px-40 mb-5"></div>
+                )}
+              </div>
+              <div className="text-center">
+                <h1 className="text-3xl mb-5">Weakness</h1>
+                <div className="grid grid-rows-2 text-center gap-y-5">
+                  <div className="grid grid-cols-2 gap-x-3">
+                    <p className="border rounded-full py-1">Fire</p>
+                    <p className="border rounded-full py-1">Ice</p>
+                  </div>
+                  <div className="grid grid-cols-2 gap-x-3">
+                    <p className="border rounded-full py-1">Flying</p>
+                    <p className="border rounded-full py-1">Psychic</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="relative flex justify-center items-center top-16 -z-10">
+              <img
+                className="absolute animate-spin-custom"
+                src="https://id.portal-pokemon.com/play/resources/pokedex/img/pokemon_bg.png"
+                alt=""
+              />
+              <img
+                className="absolute w-[350px]"
+                src="https://id.portal-pokemon.com/play/resources/pokedex/img/pokemon_circle_bg.png"
+                alt=""
+              />
+              <img
+                className="absolute w-[50%]"
+                src={
+                  profilePokemon.sprites?.other["official-artwork"]
+                    ?.front_default
+                }
+                alt={profilePokemon.name}
+              />
+            </div>
+            <div className="grid text-xl px-10 mt-20 me-20 mb-10 mr-10">
+              <div className="grid grid-cols-2">
+                <div className="ml-5">
+                  <p className="text-[#B0E5F9]">Height</p>
+                  <p>{formattedHeight} m</p>
+                </div>
+                <div>
+                  <p className="text-[#B0E5F9]">Base Experience</p>
+                  <p>{profilePokemon.base_experience}</p>
+                </div>
+              </div>
+              <div className="grid grid-cols-2">
+                <div className="ml-5">
+                  <p className="text-[#B0E5F9]">Weight</p>
+                  <p>{formattedWeight} kg</p>
+                </div>
+                <div>
+                  <p className="text-[#B0E5F9]">Abilities</p>
+                  {profilePokemon.abilities?.map((ability) => {
+                    const formattedAbility = ability.ability.name
+                      .split("-")
+                      .map(
+                        (word) =>
+                          word.charAt(0).toUpperCase() +
+                          word.slice(1).toLowerCase()
+                      )
+                      .join(" ");
+                    return <p key={ability.ability.name}>{formattedAbility}</p>;
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-3">
+          <div className="ps-20">
+            <h1 className="text-3xl text-[#B0E5F9]">Version</h1>
+            <p className="text-xl mt-5">
+              Mandi cahaya matahari membuat Ivysaur makin kuat dan menumbuhkan
+              kuncup di punggungnya.
+            </p>
+          </div>
+          <div></div>
+          <div>
+            <h1 className="text-3xl text-[#B0E5F9]">Statistics</h1>
+            <div className="mt-5">
+              {profilePokemon.stats?.map((stat) => {
+                const formattedStats = stat.stat.name
+                  .split("-")
+                  .map(
+                    (word) =>
+                      word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+                  )
+                  .join(" ");
+                return (
+                  <div className="flex gap-3 text-xl" key={stat.stat.name}>
+                    <h1>{formattedStats}</h1>
+                    <p>{stat.base_stat}</p>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center">
+          <div className="text-center border border-[#466E9B] rounded-full w-[50%] text-lg text-[#b3eafe] hover:bg-[#B4EBFF] hover:text-black hover:border-transparent">
+            <Link to="/">
+              <button className="w-full p-2">Back to Home Page</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default ProfilePokemonPage
+export default ProfilePokemonPage;
--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,66 +1,84 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const colorButton = {
-    normal: 'bg-[#A0A0A0] border-[#DCDCDC]',
-    fighting: 'bg-[#C85500] border-[#DC6900]',
-    flying: 'bg-[#79BCD7] border-[#78DCFF]',
-    poison: 'bg-[#BE78BE] border-[#D28CD2]',
-    ground: 'bg-[#CCA142] border-[#F9C75A]',
-    rock: 'bg-[#A07850] border-[#B48C64]',
-    bug: 'bg-[#32B432] border-[#46C846]',
-    ghost: 'bg-[#8C78F0] border-[#A08CFF]',
-    steel: 'bg-[#96B4DC] border-[#AAC8F0]',
-    fire: 'bg-[#FF3700] border-[#FF6900]',
-    water: 'bg-[#0094E5] border-[#14B9FF]',
-    grass: 'bg-[#92BF19] border-[#B4F000]',
-    electric: 'bg-[#E4B700] border-[#FFE100]',
-    psychic: 'bg-[#DC78C8] border-[#EF8BDB]',
-    ice: 'bg-[#00B7EE] border-[#14F5FF]',
-    dragon: 'bg-[#3C64C8] border-[#5078DC]',
-    dark: 'bg-[#606060] border-[#787878]',
-    fairy: 'bg-[#FF7EB8] border-[#FDAEDA]',
-    stellar: 'bg-[#A0A0A0]',
-    shadow: 'bg-[#A0A0A0]',
-    unknown: 'bg-[#A0A0A0]'
-}
+  normal: "bg-[#A0A0A0] border-[#DCDCDC]",
+  fighting: "bg-[#C85500] border-[#DC6900]",
+  flying: "bg-[#79BCD7] border-[#78DCFF]",
+  poison: "bg-[#BE78BE] border-[#D28CD2]",
+  ground: "bg-[#CCA142] border-[#F9C75A]",
+  rock: "bg-[#A07850] border-[#B48C64]",
+  bug: "bg-[#32B432] border-[#46C846]",
+  ghost: "bg-[#8C78F0] border-[#A08CFF]",
+  steel: "bg-[#96B4DC] border-[#AAC8F0]",
+  fire: "bg-[#FF3700] border-[#FF6900]",
+  water: "bg-[#0094E5] border-[#14B9FF]",
+  grass: "bg-[#92BF19] border-[#B4F000]",
+  electric: "bg-[#E4B700] border-[#FFE100]",
+  psychic: "bg-[#DC78C8] border-[#EF8BDB]",
+  ice: "bg-[#00B7EE] border-[#14F5FF]",
+  dragon: "bg-[#3C64C8] border-[#5078DC]",
+  dark: "bg-[#606060] border-[#787878]",
+  fairy: "bg-[#FF7EB8] border-[#FDAEDA]",
+  stellar: "bg-[#A0A0A0]",
+  shadow: "bg-[#A0A0A0]",
+  unknown: "bg-[#A0A0A0]",
+};
 
 function SearchResults({ searchResults }) {
+  const navigate = useNavigate();
+  const goToPokemonProfile = (pokemonId) => {
+    navigate(`/profilepokemon/${pokemonId}`);
+  };
 
-    const navigate = useNavigate()
-    const goToPokemonProfile = (pokemonId) => {
-        navigate(`/profilepokemon/${pokemonId}`)
-    }
+  const setButtonColor = (type) => {
+    return colorButton[type];
+  };
 
-    const setButtonColor = (type) => {
-        return colorButton[type]
-    }
-
-    const formattedId = searchResults.id.toString().padStart(4, '0')
-    const formattedName = searchResults.name.charAt(0).toUpperCase() + searchResults.name.slice(1).toLowerCase()
-    return (
-        <div className='grid grid-cols-4 gap-5'>
-            <div className='grid border border-[#466E9B] py-4 rounded-3xl bg-[#0A141E] text-white cursor-pointer' onClick={() => goToPokemonProfile(searchResults.id)}>
-                <div className='flex justify-center'>
-                    <img src={searchResults.sprites?.other['official-artwork']?.front_default} alt={searchResults.name} />
-                </div>
-                <div className='grid gap-y-10 px-5'>
-                    <div>
-                        <h1 className='text-[#B4EBFF]'>{formattedId}</h1>
-                        <h1 className='text-xl font-semibold'>{formattedName}</h1>
-                    </div>
-                    <div className='grid grid-cols-2 text-center gap-x-5'>
-                        {searchResults.types?.map((type) => {
-                            const formattedType = type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1).toLowerCase()
-                            return (
-                                <h1 className={`border rounded-full py-1 ${setButtonColor(type.type.name)}`} key={type.slot}>{formattedType}</h1>
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
+  const formattedId = searchResults.id.toString().padStart(4, "0");
+  const formattedName =
+    searchResults.name.charAt(0).toUpperCase() +
+    searchResults.name.slice(1).toLowerCase();
+  return (
+    <div className="grid grid-cols-4 gap-5">
+      <div
+        className="grid border border-[#466E9B] py-4 rounded-3xl bg-[#0A141E] text-white cursor-pointer"
+        onClick={() => goToPokemonProfile(searchResults.id)}
+      >
+        <div className="flex justify-center">
+          <img
+            src={
+              searchResults.sprites?.other["official-artwork"]?.front_default
+            }
+            alt={searchResults.name}
+          />
         </div>
-    )
+        <div className="grid gap-y-10 px-5">
+          <div>
+            <h1 className="text-[#B4EBFF]">{formattedId}</h1>
+            <h1 className="text-xl font-semibold">{formattedName}</h1>
+          </div>
+          <div className="grid grid-cols-2 text-center gap-x-5">
+            {searchResults.types?.map((type) => {
+              const formattedType =
+                type.type.name.charAt(0).toUpperCase() +
+                type.type.name.slice(1).toLowerCase();
+              return (
+                <h1
+                  className={`border rounded-full py-1 ${setButtonColor(
+                    type.type.name
+                  )}`}
+                  key={type.slot}
+                >
+                  {formattedType}
+                </h1>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default SearchResults
+export default SearchResults;
