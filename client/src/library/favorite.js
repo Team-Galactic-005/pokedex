92a333daa34e57220d537b1c5c207698be174470
JeremyDezekiel <jeremydezekiel@gmail.com>
2024-12-20 17:22:24 +0700

    serverGlitch

--- client/src/library/favorite.js
+++ client/src/library/favorite.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const favoriteAPI = axios.create({
-    baseURL: 'http://localhost:3000'
+    // baseURL: 'http://localhost:3000'
+    baseURL: 'https://meadow-dashing-tarsal.glitch.me/api'
 })
 
 export default favoriteAPI
